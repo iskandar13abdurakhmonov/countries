70bd087bebb9b9f43836a3f619ae69656e332854
Iskandar Abdurakhmonov <iskandar.13.aburahmonov@gmail.com>
2023-10-31 20:53:46 +0500

    initializing semantic layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,23 @@ function App() {
 
   return (
     <div className="App">
-      Countries
+      <div className="wrapper">
+        <header className="header">
+          <div className="header__container">
+            header
+          </div>
+        </header>
+        <main className="main">
+          <div className="main__container">
+            main
+          </div>
+        </main>
+        <footer className="footer">
+          <div className="footer__container">
+            footer
+          </div>
+        </footer>
+      </div>
     </div>
   )
 }
