983762a6456ca0bd00f196b7bc4f1cee035b84e1
Iskandar Abdurakhmonov <iskandar.13.aburahmonov@gmail.com>
2023-11-02 14:09:03 +0500

    header section static styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
 import styles from './Header.module.css'
+import { HiOutlineMoon } from 'react-icons/hi'
 
 export default function Header() {
     return (
         <header className={styles.header}>
-            <div className="header__container">header</div>
+            <div className={`header__container ${styles.headerContainer}`}>
+                <span className={styles.logo}>Where in the world?</span>
+                <button className={styles.themeToggle}>
+                    <HiOutlineMoon />
+                    Dark Mode
+                </button>
+            </div>
         </header>
     )
 }
