aeb5588c2789c33d8119004294800aeba48d9542
Iskandar Abdurakhmonov <iskandar.13.aburahmonov@gmail.com>
2023-11-02 13:14:32 +0500

    fetching data from an API

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
 import Header from './components/Header'
 import { BrowserRouter as Rounter, Routes, Route } from 'react-router-dom'
-import Home from './components/Home'
-import Country from './components/Country'
+import Home from './pages/Home'
+import Country from './pages/Country'
 import Footer from './components/Footer'
+import { useEffect, useState } from 'react'
 
 function App() {
+    const [countries, setCountriers] = useState([])
+    const [country, setCountry] = useState({})
+    const [error, setError] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+
+    console.log(country)
+
+    useEffect(() => {
+        const getCountries = async () => {
+            try {
+                setIsLoading(true)
+                const res = await fetch(
+                    'https://restcountries.com/v3.1/name/USA'
+                )
+                if (!res.ok) {
+                    throw new Error(
+                        'Something went wrong with fetching the data :('
+                    )
+                }
+                const data = await res.json()
+                setCountry(data)
+            } catch (err) {
+                console.log(err)
+                setError(err.message)
+            } finally {
+                setIsLoading(false)
+            }
+        }
+        getCountries()
+    }, [])
+
     return (
         <div className="App">
             <div className="wrapper">
-                <Header/>
+                <Header />
                 <main className="main">
                     <div className="main__container">
                         <Rounter>
@@ -25,7 +57,7 @@ function App() {
                         </Rounter>
                     </div>
                 </main>
-               <Footer/>
+                <Footer />
             </div>
         </div>
     )
