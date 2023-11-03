8621f2425dc5b8bdeec10a8a4bc4487fe8038a84
Iskandar Abdurakhmonov <iskandar.13.aburahmonov@gmail.com>
2023-11-03 08:48:02 +0500

    static country page layout without css

--- src/components/CountryItem.jsx
+++ src/components/CountryItem.jsx
@@ -3,48 +3,38 @@ import { Link, useNavigate, useSearchParams } from 'react-router-dom'
 import styles from './CountryItem.module.css'
 
 export default function CountryItem({ country }) {
-   
     const [searchParams] = useSearchParams()
 
-    
-
     return (
-        <>
-            <Link to={`/country?current=${country.name.common}`}
-                className={styles.countryItem}
-            >
-                <div className={styles.imageBox}>
-                    <img
-                        src={country.flags.png}
-                        alt="germany flag"
-                    />
+        <Link
+            to={`/country?current=${country.name.common}`}
+            className={styles.countryItem}
+        >
+            <div className={styles.imageBox}>
+                <img
+                    src={country.flags.png}
+                    alt="germany flag"
+                />
+            </div>
+            <div className={styles.textBox}>
+                <h2 className={styles.countryName}>{country.name.common}</h2>
+                <div className={styles.infos}>
+                    <span className={styles.title}>
+                        Population:
+                        <span className={styles.descr}>
+                            {country.population}
+                        </span>
+                    </span>
+                    <span className={styles.title}>
+                        Region:{' '}
+                        <span className={styles.descr}>{country.region}</span>
+                    </span>
+                    <span className={styles.title}>
+                        Capital:{' '}
+                        <span className={styles.descr}>{country.capital}</span>
+                    </span>
                 </div>
-                <div className={styles.textBox}>
-                    <h2 className={styles.countryName}>
-                        {country.name.common}
-                    </h2>
-                    <div className={styles.infos}>
-                        <span className={styles.title}>
-                            Population:
-                            <span className={styles.descr}>
-                                {country.population}
-                            </span>
-                        </span>
-                        <span className={styles.title}>
-                            Region:{' '}
-                            <span className={styles.descr}>
-                                {country.region}
-                            </span>
-                        </span>
-                        <span className={styles.title}>
-                            Capital:{' '}
-                            <span className={styles.descr}>
-                                {country.capital}
-                            </span>
-                        </span>
-                    </div>
-                </div>
-            </Link>
-        </>
+            </div>
+        </Link>
     )
 }
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -2,11 +2,12 @@ import { useNavigate, useSearchParams } from 'react-router-dom'
 import styles from './Country.module.css'
 import { useEffect, useState } from 'react'
 
-export default function Country() {
+import { BsArrowLeft } from 'react-icons/bs'
 
+export default function Country() {
     const [searchParams] = useSearchParams()
     const current = searchParams.get('current')
-    
+
     const [country, setCountry] = useState({})
     const [error, setError] = useState('')
     const [isLoading, setIsLoading] = useState(false)
@@ -16,37 +17,99 @@ export default function Country() {
     console.log(country)
 
     useEffect(() => {
-      const getCountries = async () => {
-          try {
-              setIsLoading(true)
-              const res = await fetch(
-                  `https://restcountries.com/v3.1/name/${current}`
-              )
-              if (!res.ok) {
-                  throw new Error(
-                      'Something went wrong with fetching the data :('
-                  )
-              }
-              const data = await res.json()
-              setCountry(data[0])
-          } catch (err) {
-              console.log(err)
-              setError(err.message)
-          } finally {
-              setIsLoading(false)
-          }
-      }
-      getCountries()
-  }, [current])
+        const getCountries = async () => {
+            try {
+                setIsLoading(true)
+                const res = await fetch(
+                    `https://restcountries.com/v3.1/name/${current}`
+                )
+                if (!res.ok) {
+                    throw new Error(
+                        'Something went wrong with fetching the data :('
+                    )
+                }
+                const data = await res.json()
+                setCountry(data[0])
+            } catch (err) {
+                console.log(err)
+                setError(err.message)
+            } finally {
+                setIsLoading(false)
+            }
+        }
+        getCountries()
+    }, [current])
 
-  const back = () => {
-    navigate(-1)
-  }
+    const back = () => {
+        navigate(-1)
+    }
 
-    return <>
-      <button onClick={() => back()}>back</button>
-      {country ? <div className={styles.country}>
-        <h1>{country?.name?.common}</h1>
-      </div> : 'Loading...'}
-    </>
+    return (
+        <div className={styles.country}>
+            <div className={styles.top}>
+                <button
+                    className={styles.backBtn}
+                    onClick={() => back()}
+                >
+                    <BsArrowLeft />
+                    Back
+                </button>
+            </div>
+            <div className={styles.box}>
+                <div className={styles.imageBackground}>
+                    <img
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Flag_of_Belgium_%28civil%29.svg/640px-Flag_of_Belgium_%28civil%29.svg.png"
+                        alt="The Flag of Belgium"
+                    />
+                </div>
+                <div className={styles.info}>
+                    <h2 className={styles.countryName}>Belgium</h2>
+                    <div className={styles.infoBox}>
+                        <div className={styles.infoLeft}>
+                            <span>
+                                Native Name: <span>Belgie</span>
+                            </span>
+                            <span>
+                                Population: <span>11,319,511</span>
+                            </span>
+                            <span>
+                                Region: <span>Europe</span>
+                            </span>
+                            <span>
+                                Sub Region: <span>Westren Europe</span>
+                            </span>
+                            <span>
+                                Capital: <span>Brussels</span>
+                            </span>
+                        </div>
+                        <div className={styles.infoRight}>
+                            <span>
+                                Top Level Domain: <span>.be</span>
+                            </span>
+                            <span>
+                                Currencies: <span>Euro</span>
+                            </span>
+                            <span>
+                                Languages: <span>Dutch, French, German</span>
+                            </span>
+                        </div>
+                    </div>
+                    <div className={styles.borderCountriesBox}>
+                        <span>Border Countries: </span>
+                        <div className={styles.borderCountries}>
+                            <button className={styles.borderCountry}>
+                                France
+                            </button>
+                            <button className={styles.borderCountry}>
+                                German
+                            </button>
+                            <button className={styles.borderCountry}>
+                                Netherlands
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
