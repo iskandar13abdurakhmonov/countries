fba7284ccd9a7bc709f41fa847acb57ab541aee0
Iskandar Abdurakhmonov <iskandar.13.aburahmonov@gmail.com>
2023-11-04 23:05:51 +0500

    applied fetching data by codes when clicking one of the borders, by default its sending request to name parameter however bordering countries only comes with string withe length of 3 for example uzbekistan will be UZB, so if current parameters lengths === 3 it will send request to code paramentere else will send to name parameter

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,19 +1,27 @@
-import { useNavigate, useSearchParams } from 'react-router-dom'
+import { Link, useNavigate, useSearchParams } from 'react-router-dom'
 import styles from './Country.module.css'
 
 import { BsArrowLeft } from 'react-icons/bs'
 import { useEffect, useState } from 'react'
 
+const NAME_URL = 'https://restcountries.com/v3.1/name/'
+
+const CODE_URL = 'https://restcountries.com/v3.1/alpha/'
+
 export default function Country() {
     const [country, setCountry] = useState({})
     const [error, setError] = useState('')
     const [isLoading, setIsLoading] = useState(false)
+    const languages = country.languages
+    const borderingCountries = country.borders
 
     const navigate = useNavigate()
 
     const [searchParams] = useSearchParams()
 
     const current = searchParams.get('current')
+
+    console.log(current)
 
     const back = () => {
         navigate(-1)
@@ -24,15 +32,17 @@ export default function Country() {
             setIsLoading(true)
             try {
                 const res = await fetch(
-                    `https://restcountries.com/v3.1/name/${current}`
+                    `${current.length === 3 ? CODE_URL : NAME_URL}${current}`
                 )
 
-                if(!res.ok) {
-                    throw new Error('Something went wrong while fetching the data :(')
+                if (!res.ok) {
+                    throw new Error(
+                        'Something went wrong while fetching the data :('
+                    )
                 }
 
                 const data = await res.json()
-                console.log(data)
+                setCountry(data[0])
             } catch (err) {
                 setError(err)
                 console.log(err.message)
@@ -57,7 +67,8 @@ export default function Country() {
             <div className={styles.box}>
                 <img
                     className={styles.imageBackground}
-                    alt="The Flag of Belgium"
+                    src={country?.flags?.png}
+                    alt={country?.flags?.alt}
                 />
                 <div className={styles.info}>
                     <h2 className={styles.countryName}></h2>
@@ -65,35 +76,69 @@ export default function Country() {
                         <div className={styles.infoLeft}>
                             <span className={styles.title}>
                                 Native Name:{' '}
-                                <span className={styles.descr}></span>
+                                <span className={styles.descr}>
+                                    {country?.name?.common}
+                                </span>
                             </span>
                             <span className={styles.title}>
                                 Population:{' '}
-                                <span className={styles.descr}></span>
+                                <span className={styles.descr}>
+                                    {country?.population}
+                                </span>
                             </span>
                             <span className={styles.title}>
-                                Region: <span className={styles.descr}></span>
+                                Region:{' '}
+                                <span className={styles.descr}>
+                                    {country?.region}
+                                </span>
                             </span>
                             <span className={styles.title}>
                                 Sub Region:{' '}
-                                <span className={styles.descr}></span>
+                                <span className={styles.descr}>
+                                    {country?.subregion}
+                                </span>
                             </span>
                             <span className={styles.title}>
-                                Capital: <span className={styles.descr}></span>
+                                Capital:{' '}
+                                <span className={styles.descr}>
+                                    {country?.capital}
+                                </span>
                             </span>
                         </div>
                         <div className={styles.infoRight}>
                             <span className={styles.title}>
                                 Top Level Domain:{' '}
-                                <span className={styles.descr}></span>
+                                <span className={styles.descr}>
+                                    {country?.tld?.[0]}
+                                </span>
                             </span>
                             <span className={styles.title}>
                                 Currencies:{' '}
-                                <span className={styles.descr}></span>
+                                <span className={styles.descr}>
+                                    {country.currencies
+                                        ? Object.keys(country.currencies)
+                                              .map(
+                                                  (currencyCode) =>
+                                                      country.currencies[
+                                                          currencyCode
+                                                      ].name
+                                              )
+                                              .join(', ')
+                                        : ''}
+                                </span>
                             </span>
                             <span className={styles.title}>
                                 Languages:{' '}
-                                <span className={styles.descr}></span>
+                                {languages
+                                    ? Object.keys(languages).map((language) => (
+                                          <span
+                                              key={language}
+                                              className={styles.descr}
+                                          >
+                                              {languages[language]}
+                                          </span>
+                                      ))
+                                    : ''}
                             </span>
                         </div>
                     </div>
@@ -101,7 +146,16 @@ export default function Country() {
                         <span className={styles.borderCountriesTitle}>
                             Border Countries:{' '}
                         </span>
-                        <div className={styles.borderCountries}></div>
+                        {borderingCountries
+                            ? borderingCountries.map((borderingCountry) => (
+                                  <Link to={`/country?current=${borderingCountry}`}
+                                      key={borderingCountry}
+                                      className={styles.borderCountries}
+                                  >
+                                      {borderingCountry}
+                                  </Link>
+                              ))
+                            : ''}
                     </div>
                 </div>
             </div>
